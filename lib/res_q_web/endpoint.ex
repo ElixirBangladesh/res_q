c18ac322f8db545839a2e91ad8bfced7aca3add7
kamranhossain <kamran.hossain227@gmail.com>
2020-07-07 15:24:05 +0600

    add pow session plug in endpoint

--- lib/res_q_web/endpoint.ex
+++ lib/res_q_web/endpoint.ex
@@ -50,5 +50,6 @@ defmodule ResQWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :res_q
   plug ResQWeb.Router
 end
